c43d1488c09f3529588a29c315578b0d8bdc9120
rajansadhu3 <rajansadhu3@gmail.com>
2025-04-15 16:22:19 +0530

    Base food app

---
+++ App.js
@@ -0,0 +1,126 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/* 
+Header 
+  - Logo
+  - Nav Items
+Body 
+  - Search 
+  - Resturant Container
+      - Resturant Cards
+Footer
+  - Copyright
+  - Links
+  - Address
+  - Contact 
+*/
+
+const Header = () => {
+  return (
+    <div className="header flex justify-space-between align-centre">
+      <div className="logo">
+        <img
+          alt="logo"
+          width={100}
+          src="https://1000logos.net/wp-content/uploads/2021/05/Swiggy-logo.png"
+        />
+      </div>
+      <div className="navItems list-items">
+        <ul className="no-list-style flex">
+          <li>
+            <a className="text-decor-none" href="/">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="text-decor-none" href="/">
+              About Us
+            </a>
+          </li>
+          <li>
+            <a className="text-decor-none" href="/">
+              Our Profile
+            </a>
+          </li>
+          <li>
+            <a className="text-decor-none" href="/">
+              Our Services
+            </a>
+          </li>
+          <li>
+            <a className="text-decor-none" href="/">
+              Cart
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span className="copyright">© 2025 Swiggy Limited</span>
+    </div>
+  );
+};
+
+const Search = () => {
+  return (
+    <div className="search">
+      <input type="text" />
+      <button type="submit">Search</button>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <Search />
+      <ResturantContainer />
+    </div>
+  );
+};
+
+const Resturant = () => {
+  return (
+    <div className="resturantCard">
+      <div className="topImage">
+        <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/4/9/7cbf96b9-d578-44e8-a3c5-cd8ce2facc22_220652.jpg" />
+      </div>
+      <div className="bottomContent">
+        <h2>Resturnat Name</h2>
+        <h3>Location : Gandhinagar</h3>
+        <p>Birayani, pizza, food</p>
+        <p>Ratings: 4.4s</p>
+      </div>
+    </div>
+  );
+};
+const ResturantContainer = () => {
+  return (
+    <div className="resContainer">
+      <Resturant />
+      <Resturant />
+      <Resturant />
+      <Resturant />
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
